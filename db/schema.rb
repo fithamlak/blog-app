de9e9481764f3d1f75dd561ed094dbe33edbc47d
fithamlak <justiceofgod01@gmail.com>
2023-05-11 00:49:42 +0300

    little change on comment migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_10_003943) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_10_211439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.bigint "post_id", null: false
     t.text "text"
-    t.bigint "author_id", null: false
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
