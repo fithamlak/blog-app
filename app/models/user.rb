45cd311cd1220adf7fd8858b62bd5c5b791681d9
fithamlak <justiceofgod01@gmail.com>
2023-05-25 09:57:47 +0300

    Update users index view to desplay daynamic data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_validation :set_posts_counter, on: :create
   has_many :posts, foreign_key: :author_id, dependent: :destroy
   has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :likes, foreign_key: :author_id, dependent: :destroy
@@ -7,6 +8,12 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def last_three_posts
-    posts.order(created_at: :desc).limit(3)
+    posts.order(created_at: :desc).limit(3).reserse_order
   end
+
+  private
+
+  def set_posts_counter
+    self.posts_counter = 0
+  end  
 end
