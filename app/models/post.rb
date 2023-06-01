e13e6582e6aa5664740815a06e7dfba3ae83ddcf
fithamlak <justiceofgod01@gmail.com>
2023-06-01 12:02:02 +0300

    add little change in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,12 +7,11 @@ class Post < ApplicationRecord
   before_validation :set_comments_counter, on: :create
   before_validation :set_likes_counter, on: :create
   after_save :update_author_posts_counter
+  after_destroy :decrement_author_posts_counter
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter,
-            numericality: { only_integer: true, greater_than_or_equal_to: 0,
-                            message: 'must be greater than or equal to zero' }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   private
 
@@ -21,10 +20,14 @@ class Post < ApplicationRecord
   end
 
   def set_likes_counter
-    self.likes_counter ||= 0
+    self.likes_counter || = 0
   end
 
   def update_author_posts_counter
     author.increment!(:posts_counter)
+  end
+
+  def decrement_author_posts_counter
+    author.decrement!(:posts_counter)
   end
 end
