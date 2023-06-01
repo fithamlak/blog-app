2f0ff9899a72d595a5b44d4963f28abba0d01fae
Vanessa <vaneolipa@gmail.com>
2023-06-01 10:14:32 -0500

    Create seeds.rb that deletes existing records and creates new data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,60 +2,52 @@ User.destroy_all
 Post.destroy_all
 Comment.destroy_all
 Like.destroy_all
-
 user1 = User.create!(
-  email: 'test1@test.com',
+  email: 'ordinaluser1@test.com',
   password: 'password1',
   password_confirmation: 'password1',
   confirmed_at: Time.now,
-  name: Faker::Name.first_name, 
-  photo: Faker::Avatar.image, 
+  name: Faker::Name.first_name,
+  photo: Faker::Avatar.image,
   bio: Faker::Job.title
 )
-
 post1 = Post.create!(
-  title: Faker::Quotes::Shakespeare.hamlet_quote, 
+  title: Faker::Quotes::Shakespeare.hamlet_quote,
   text: Faker::Quote.matz,
   author: user1
 )
-
 comment1 = Comment.create!(
   text: Faker::Quote.matz,
-  post: post1, 
+  post: post1,
   author: user1
 )
-
 comment2 = Comment.create!(
   text: Faker::Quote.matz,
-  post: post1, 
+  post: post1,
   author: user1
 )
-
 post2 = Post.create!(
-  title: Faker::Quotes::Shakespeare.hamlet_quote, 
+  title: Faker::Quotes::Shakespeare.hamlet_quote,
   text: Faker::Quote.matz,
   author: user1
 )
-
 user2 = User.create!(
-  email: 'admin@test.com',
+  email: 'adminuser@test.com',
   password: 'password',
   password_confirmation: 'password',
   confirmed_at: Time.now,
-  name: Faker::Name.first_name, 
-  photo: Faker::Avatar.image, 
+  name: Faker::Name.first_name,
+  photo: Faker::Avatar.image,
   bio: Faker::Job.title,
   role: 'admin'
 )
-
 post3 = Post.create!(
-  title: Faker::Quotes::Shakespeare.hamlet_quote, 
+  title: Faker::Quotes::Shakespeare.hamlet_quote,
   text: Faker::Quote.matz,
   author: user2
 )
-
 comment3 = Comment.create!(
   text: Faker::Quote.matz,
-  post: post1, 
+  post: post1,
   author: user2
 )
